6d4268169780ae82fff59bbe6ac9e6adb0098198
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2023-07-08 12:43:48 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = https://movies-downloader-bot-ten-phi.vercel.app
+URL = https://movies-downloader-bot.vercel.app
 bot = Bot(TOKEN)
 
 
